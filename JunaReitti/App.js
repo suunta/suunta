210c13dbc26054447f3729c36779c72b27487b56
welski <welc@welc.net>
2018-02-13 11:41:16 +0200

    ListView muutettu -> FlatList, pull-to-refresh (onRefresh) toimii välillä?

--- JunaReitti/App.js
+++ JunaReitti/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {ActivityIndicator, View, Text, ListView, TextInput, TouchableOpacity, StyleSheet} from "react-native";
+import {ActivityIndicator, View, Text, ListView, TextInput, TouchableOpacity, StyleSheet, FlatList} from "react-native";
 import {List, ListItem} from "react-native-elements";
 
 //import Autocomplete from 'react-native-autocomplete-input'; // 3.3.1
@@ -10,11 +10,13 @@ export default class JunaReitti extends Component<{}> {
         super(props);
 
         this.state = {
-            isLoading: true
+            data: [],
+            isLoading: true,
+            isRefreshing: false
         }
     }
 
-    componentDidMount() {
+    fetchTrainData() {
         return fetch('https://rata.digitraffic.fi/api/v1/live-trains/station/PSL/KE')
             .then((response) => response.json())
             .then(junat => junat.map(juna => {
@@ -27,12 +29,11 @@ export default class JunaReitti extends Component<{}> {
                     }
                 })
             )
-            //.then(junat => junat.filter(juna => juna.aikaTauluRivit.includes('DEPARTURE')))
             .then((responseJson) => {
-                let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+                //let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
                 this.setState({
                     isLoading: false,
-                    dataSource: ds.cloneWithRows(responseJson),
+                    data: responseJson,
                 }, function () {
                     // do something with new state
                 });
@@ -42,11 +43,60 @@ export default class JunaReitti extends Component<{}> {
             });
     }
 
+    componentDidMount() {
+        return this.fetchTrainData();
+    }
+
+    onRefresh = async () => {
+        this.setState({
+            isRefreshing: true
+        });
+
+        //await this.fetchTrainData();
+
+        await this.setState({
+            data: this.fetchTrainData()
+        });
+
+        this.setState({
+            isRefreshing: false
+        });
+
+    };
+
+    renderHeader() {
+        return <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center'
+        }}>
+            <Text>Tunnus</Text>
+            <Text>Lähtöaika</Text>
+            <Text>Lähtöraide</Text>
+            <Text>Tuloaika</Text>
+        </View>
+    };
+
+    renderItem({ item, index }) {
+        return <View style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    alignItems: 'center'
+                }}>
+                <Text>{item.tunnus}</Text>
+                <Text>{item.lahtoAika}</Text>
+                <Text>{item.lahtoRaide}</Text>
+                <Text>{item.tuloAika}</Text>
+        </View>;
+    }
+
     render() {
 
         if (this.state.isLoading) {
             return (
-                <View style={{flex: 1, paddingTop: 20}}>
+                <View style={{flex: 1, paddingTop: 40}}>
                     <ActivityIndicator/>
                 </View>
             );
@@ -54,15 +104,26 @@ export default class JunaReitti extends Component<{}> {
 
 
         return (
-            <View style={{flex: 1, paddingTop: 20}}>
-                <Text>id | Tunnus | Lähtöaika | Raide | Tuloaika</Text>
+            <View style={{flex: 1, paddingTop: 0}}>
+                {/*<Text>id | Tunnus | Lähtöaika | Raide | Tuloaika</Text>*/}
                 <List>
+                    <FlatList
+                        data = {this.state.data}
+                        keyExtractor = {item => item.id.toString()}
+                        ListHeaderComponent = {this.renderHeader}
+                        renderItem = {this.renderItem}
+                        onRefresh={this.onRefresh}
+                        refreshing={this.state.isRefreshing}
+                    />
+                </List>
+                {/*
                     <ListView
                         dataSource={this.state.dataSource}
                         renderRow={(rowData) =>
                             <Text>{rowData.id} | {rowData.tunnus} | {rowData.lahtoAika} | {rowData.lahtoRaide} | {rowData.tuloAika} </Text>}
                     />
                 </List>
+                */}
             </View>
         );
     }
