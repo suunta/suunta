15de637a0dece669feda6335b3e7229116a4c8bd
Marion Karlsson <marion.karlsson@gmail.com>
2018-03-06 13:09:12 +0200

    latausindikaattori toimimaan asemahakuun

--- JunaReitti/App.js
+++ JunaReitti/App.js
@@ -21,8 +21,11 @@ export default class JunaReitti extends Component<{}> {
     }
 
     fetchTrainData = () => {
+        this.setState({
+            isRefreshing: true
+        });
         if(this.state.tuloLyhenne !== '' && this.state.lahtoLyhenne !== '') {
-            console.log("HAKEE " + this.state.lahtoLyhenne + " " + this.state.tuloLyhenne);
+            console.log("hakee " + this.state.lahtoLyhenne + " " + this.state.tuloLyhenne);
             fetch('https://rata.digitraffic.fi/api/v1/live-trains/station/'+this.state.lahtoLyhenne+'/'+this.state.tuloLyhenne)
                 .then((response) => response.json())
                 .then(junat => junat.map(juna => {
@@ -41,6 +44,7 @@ export default class JunaReitti extends Component<{}> {
                     this.setState({
                         isLoading: false,
                         data: responseJson,
+                        isRefreshing: false,
                     }, function () {
                         // do something with new state
                     });
@@ -114,7 +118,7 @@ export default class JunaReitti extends Component<{}> {
                     return {
                         id: asema.stationUICCode,
                         stationShortCode: asema.stationShortCode,
-                        stationName: asema.stationName,
+                        stationName: asema.stationName.split(" ")[1] === "asema" ? asema.stationName.split(" ")[0] : asema.stationName,
                         passengerTraffic: asema.passengerTraffic
                     }
                 })
@@ -143,31 +147,25 @@ export default class JunaReitti extends Component<{}> {
     };
 
     renderHeader() {
-        return <View style={{
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center'
-        }}>
-            <Text>Tunnus</Text>
-            <Text>Lähtöaika</Text>
-            <Text>Lähtöraide</Text>
-            <Text>Tuloaika</Text>
-        </View>
+        return (
+            <View style={styles.junalista}>
+                <Text>Tunnus</Text>
+                <Text>Lähtöaika</Text>
+                <Text>Lähtöraide</Text>
+                <Text>Tuloaika</Text>
+            </View>
+        );
     };
 
-    renderItem({ item, index }) {
-        return <View style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                    alignItems: 'center'
-                }}>
+    renderItem({item, index}) {
+        return (
+            <View style={styles.junalista}>
                 <Text>{item.tunnus}</Text>
                 <Text>{item.lahtoAika}</Text>
                 <Text>{item.lahtoRaide}</Text>
                 <Text>{item.tuloAika}</Text>
-        </View>;
+            </View>
+        );
     }
 
     render() {
@@ -184,14 +182,14 @@ export default class JunaReitti extends Component<{}> {
             <View style={{flex: 1, paddingTop: 0}}>
 
                 <View style={styles.inputContainer}>
-                <Input userInput={this.handleDepartInput}/>
-                <Input userInput={this.handleDestInput}/>
+                <Input placeholder="Lähtöasema" userInput={this.handleDepartInput}/>
+                <Input placeholder="Tuloasema" userInput={this.handleDestInput}/>
                 </View>
-                <Text>{this.state.lahtoAsema}</Text>
+                {/*<Text>{this.state.lahtoAsema}</Text>
                 <Text>{this.state.lahtoLyhenne}</Text>
 
                 <Text>{this.state.tuloAsema}</Text>
-                <Text>{this.state.tuloLyhenne}</Text>
+                <Text>{this.state.tuloLyhenne}</Text>*/}
 
                 <List>
                     <FlatList
@@ -221,5 +219,19 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center'
+    },
+    junalista: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: 50
+    },
+    junat: {
+        fontSize: 20
+    },
+    junatHeader: {
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 });
