017eec89676ab17a5746a5e0a8ee60e29c86dcc8
Kontinen Jeremias <a1602691@myy.haaga-helia.fi>
2018-03-27 13:30:00 +0300

    swiper alku

--- JunaReitti/App.js
+++ JunaReitti/App.js
@@ -3,6 +3,7 @@ import {ActivityIndicator, View, Text, StyleSheet, FlatList} from "react-native"
 import {List, ListItem} from "react-native-elements";
 import Input from "./Components/Input";
 import sortBy from "lodash/sortBy";
+import Swiper from 'react-native-swiper';
 
 export default class JunaReitti extends Component<{}> {
 
@@ -13,8 +14,8 @@ export default class JunaReitti extends Component<{}> {
             data: [],
             isLoading: true,
             isRefreshing: false,
-            lahtoAsema: '',
-            tuloAsema: '',
+            lahtoAsema: [],
+            tuloAsema: [],
             lahtoLyhenne: '',
             tuloLyhenne: '',
             asemat: [],
@@ -201,36 +202,66 @@ export default class JunaReitti extends Component<{}> {
         }
 
         return (
-            <View style={{flex: 1, paddingTop: 0}}>
+            <Swiper style={styles.wrapper} showsButtons={false}>
+                <View style1={{flex: 1, paddingTop: 0}}>
+                    <View style={styles.inputContainer}>
+                        <Input placeholder="Lähtöasema" userInput={this.handleDepartInput}/>
+                        <Input placeholder="Tuloasema" userInput={this.handleDestInput}/>
+                    </View>
+                    {/*<Text>{this.state.lahtoAsema}</Text>
+                    <Text>{this.state.lahtoLyhenne}</Text>
+                    <Text>{this.state.tuloAsema}</Text>
+                    <Text>{this.state.tuloLyhenne}</Text>*/}
 
-                <View style={styles.inputContainer}>
-                <Input placeholder="Lähtöasema" userInput={this.handleDepartInput}/>
-                <Input placeholder="Tuloasema" userInput={this.handleDestInput}/>
+                    <List>
+                        <FlatList
+                            data = {sortBy(this.state.data, 'lahtoPvm').filter(juna => juna.matkaAika < this.state.minimiAika*2.1)}
+                            keyExtractor = {item => item.id.toString()}
+                            ListHeaderComponent = {this.renderHeader}
+                            renderItem = {this.renderItem}
+                            onRefresh={this.onRefresh}
+                            refreshing={this.state.isRefreshing}
+                        />
+                    </List>
+                    {/*
+                        <ListView
+                            dataSource={this.state.dataSource}
+                            renderRow={(rowData) =>
+                                <Text>{rowData.id} | {rowData.tunnus} | {rowData.lahtoAika} | {rowData.lahtoRaide} | {rowData.tuloAika} </Text>}
+                        />
+                    </List>
+                    */}
                 </View>
-                {/*<Text>{this.state.lahtoAsema}</Text>
-                <Text>{this.state.lahtoLyhenne}</Text>
-                <Text>{this.state.tuloAsema}</Text>
-                <Text>{this.state.tuloLyhenne}</Text>*/}
+                <View style2={{flex: 1, paddingTop: 0}}>
+                    <View style={styles.inputContainer}>
+                        <Input placeholder="Lähtöasema" userInput={this.handleDepartInput}/>
+                        <Input placeholder="Tuloasema" userInput={this.handleDestInput}/>
+                    </View>
+                    {/*<Text>{this.state.lahtoAsema}</Text>
+                    <Text>{this.state.lahtoLyhenne}</Text>
+                    <Text>{this.state.tuloAsema}</Text>
+                    <Text>{this.state.tuloLyhenne}</Text>*/}
 
-                <List>
-                    <FlatList
-                        data = {sortBy(this.state.data, 'lahtoPvm').filter(juna => juna.matkaAika < this.state.minimiAika*2.1)}
-                        keyExtractor = {item => item.id.toString()}
-                        ListHeaderComponent = {this.renderHeader}
-                        renderItem = {this.renderItem}
-                        onRefresh={this.onRefresh}
-                        refreshing={this.state.isRefreshing}
-                    />
-                </List>
-                {/*
-                    <ListView
-                        dataSource={this.state.dataSource}
-                        renderRow={(rowData) =>
-                            <Text>{rowData.id} | {rowData.tunnus} | {rowData.lahtoAika} | {rowData.lahtoRaide} | {rowData.tuloAika} </Text>}
-                    />
-                </List>
-                */}
-            </View>
+                    <List>
+                        <FlatList
+                            data = {sortBy(this.state.data, 'lahtoPvm').filter(juna => juna.matkaAika < this.state.minimiAika*2.1)}
+                            keyExtractor = {item => item.id.toString()}
+                            ListHeaderComponent = {this.renderHeader}
+                            renderItem = {this.renderItem}
+                            onRefresh={this.onRefresh}
+                            refreshing={this.state.isRefreshing}
+                        />
+                    </List>
+                    {/*
+                        <ListView
+                            dataSource={this.state.dataSource}
+                            renderRow={(rowData) =>
+                                <Text>{rowData.id} | {rowData.tunnus} | {rowData.lahtoAika} | {rowData.lahtoRaide} | {rowData.tuloAika} </Text>}
+                        />
+                    </List>
+                    */}
+                </View>
+            </Swiper>
         );
     }
 }
