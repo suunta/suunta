d6c9e20d97b114f3c89df0be262f3fcd2a292eb7
make91 <make91@gmail.com>
2018-04-03 14:30:53 +0300

    asetuksissa on input laatikot
    input laatikot asetuksiin

--- JunaReitti/Asetukset.js
+++ JunaReitti/Asetukset.js
@@ -6,11 +6,18 @@ import Input from "./Components/Input";
 export default class Asetukset extends Component {
      constructor(props) {
         super(props);
+        this.state = {
+            lahto: '',
+            tulo: ''
+        }
      }
+    handleInput = (type, userInput) => {
+		return;
+    }
     
      render() {
-         const lista = this.props.asemaValit.map(vali => (
-                                           <View>
+         const lista = this.props.asemaValit.map((vali, index) => (
+                                           <View key={index}>
                                             <Text>{vali[0]}</Text>
                                             <Text>{vali[1]}</Text>
                                             <Text>Poista</Text>
@@ -18,11 +25,43 @@ export default class Asetukset extends Component {
                                         )
                                         );
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Omat reitit</Text>
+            <View style={{ flex: 1, alignItems: 'center', paddingTop: 0 }}>
+                <Text style={{ fontSize: 40}}>Omat reitit</Text>
                 {lista}
-               
+            <View style={styles.inputContainer}>
+               <Input placeholder="Lähtöasema" name="lahto" userInput={this.handleInput}/>
+                <Input placeholder="Tuloasema" name="tulo" userInput={this.handleInput}/>
+            </View>
             </View>
         );
   }
 }
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    junalista: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: 50
+    },
+    junat: {
+        fontSize: 20
+    },
+    junatHeader: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    tunnus: {
+        height: 22,
+        width: 22,
+        borderRadius: 11,
+        backgroundColor: '#EEEEEE',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
